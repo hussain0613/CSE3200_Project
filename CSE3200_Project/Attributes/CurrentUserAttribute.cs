7a8493e91d2fa21d57f6c5b8ec0cdc99296d49ff
hussain0613 <hussain0613@gmail.com>
2022-08-06 00:37:42 +0600

    Added a CurrentUserAttribute to get, authenticate and authorize current user

---
+++ CSE3200_Project/Attributes/CurrentUserAttribute.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using CSE3200_Project.Models;
+
+namespace CSE3200_Project.Attributes
+{
+    public class CurrentUserAttribute : FilterAttribute, IAuthorizationFilter
+    {
+        private DRIEntities db = new DRIEntities();
+        public bool login_required { get; set; } = false;
+        public string[] roles_required { get; set; } = { };
+
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            HttpCookie token = filterContext.HttpContext.Request.Cookies.Get("token");
+            
+            if(token != null)
+            {
+                int id = int.Parse(token["id"]);
+                User user = db.Users.Find(id);
+                if(user != null)
+                {
+                    filterContext.HttpContext.Items["current_user"] = user;
+                    if(roles_required.Length > 0 && Array.IndexOf(roles_required, user.role.ToLower()) < 0)
+                    {
+                        HttpCookie responseCookie = new HttpCookie("Message");
+                        responseCookie["message"] = "Not Authorized to view the target page!";
+                        filterContext.HttpContext.Response.Cookies.Add(responseCookie);
+                        filterContext.Result = new RedirectResult("/");
+                    }
+                    return ;
+                }
+            }
+
+            if (login_required)
+            {
+                HttpCookie responseCookie = new HttpCookie("Message");
+                responseCookie["message"] = "To view this page you need to Login first!";
+                filterContext.HttpContext.Response.Cookies.Add(responseCookie);
+                filterContext.Result = new RedirectResult($"/auth/login/?next={filterContext.HttpContext.Request.Url}");
+            }
+        }
+
+        public CurrentUserAttribute()
+        {
+        }
+
+        public CurrentUserAttribute(string role)
+        {
+            login_required = true;
+            roles_required = new string[] { role };
+        }
+        public CurrentUserAttribute(string[] rr)
+        {
+            login_required = true;
+            roles_required = rr;
+        }
+    }
+}
