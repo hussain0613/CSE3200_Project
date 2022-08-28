0faf86c90985c4ac97ccf69112e400ce88ba4489
hussain0613 <hussain0613@gmail.com>
2022-08-29 05:57:33 +0600

    added remember me functionalities

--- CSE3200_Project/Controllers/AuthController.cs
+++ CSE3200_Project/Controllers/AuthController.cs
@@ -34,7 +34,11 @@ namespace CSE3200_Project.Controllers
                 token["username"] = user.username;
                 token["id"] = user.id.ToString();
                 token["user-agent"] = Request.UserAgent;
-                token.Expires.AddHours(1);
+                if (creds.RememberMe)
+                {
+                    token.Expires = DateTime.Now;
+                    token.Expires = token.Expires.AddMonths(3);
+                }
                 // DateTime d = DateTime.Now + new TimeSpan(0,1,0);
 
                 string from = Request.QueryString["next"];
